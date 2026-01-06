ef0ce6ccb65b7f42e3c38e4a94e78804ccd1fe0b
rodolfopiresm <rodolfopiresm@gmail.com>
2026-01-06 08:31:42 -0300

    Commit com adição da solução do desafio e README com a informação do desafio

---
+++ src/index.js
@@ -0,0 +1,34 @@
+let herois = [
+    { nome: "Yuno", xp: 9500 },
+    { nome: "Asta", xp: 10500 },
+    { nome: "Noelle", xp: 2500 },
+    { nome: "Mimosa", xp: 850 },
+    { nome: "Luck", xp: 7500 }
+]
+
+for (let i = 0; i < herois.length; i++) {
+
+    let nome = herois[i].nome
+    let xp = herois[i].xp
+    let nivel = ""
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+    console.log("O Herói de " + nome + " está no nível " + nivel + ".")
+}
+
